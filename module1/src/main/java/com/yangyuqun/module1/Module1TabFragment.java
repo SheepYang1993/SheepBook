95fddd475b6009b81d15f8435d8c9df34f53a96f
SheepYang <332594623@qq.com>
2018-10-14 10:59:35 +0800

    v100 添加module1

---
+++ module1/src/main/java/com/yangyuqun/module1/Module1TabFragment.java
@@ -0,0 +1,16 @@
+package com.yangyuqun.module1;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author SheepYang
+ * @Email 332594623@qq.com
+ * @date 2018/10/14
+ */
+
+public class Module1TabFragment extends Fragment {
+    public static Module1TabFragment newInstance() {
+        Module1TabFragment fragment = new Module1TabFragment();
+        return fragment;
+    }
+}
