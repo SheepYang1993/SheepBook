a659ff727842cfbe3151bc190d156a8e546a8f44
SheepYang <332594623@qq.com>
2018-10-14 10:03:34 +0800

    Initial commit

---
+++ app/src/main/java/com/yangyuqun/sheepbook/demo/LegacyActivity.java
@@ -0,0 +1,21 @@
+package com.yangyuqun.sheepbook.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.yangyuqun.sheepbook.R;
+
+/**
+ * @author SheepYang
+ * @Email 332594623@qq.com
+ * @date 2018/10/14
+ */
+
+public class LegacyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_legacy);
+    }
+}
